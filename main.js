f14fc20cadf774e95ad0e3247b07593cfaa72553
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-05-02 00:12:17 +0300

    updated global variables

--- main.js
+++ main.js
@@ -2,3 +2,10 @@
 let selling_price = 45; //cost of milk per litre
 let shedsProduction = [510, 308, 486, 572];
 let shedsName = ["A", "B", "C", "D"];
+
+// initial value of litres
+let totalLitres = 0;
+let time;
+
+let week = 7;
+let year = 365;
