b36b868074b1da31b40b26a506aaf2b63ce438f6
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-05-02 00:21:03 +0300

    Added total production function

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ let time;
 
 let week = 7;
 let year = 365;
+
+// Calculates total production in litres per day
+let totalProduction = function(){
+    for (let i = 0; i < shedsProduction.length; i++){
+        totalLitres = totalLitres + shedsProduction[i];
+        console.log("Your production in Shed " +  shedsName[i] + " is " + shedsProduction[i] + " litres per day \n")
+    }
+    console.log("\n");
+    console.log("The total production is " + totalLitres + " litres per day\n");
+    return totalLitres;
+}
+
+totalProduction();
